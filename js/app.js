f3c56faa64262f3355318d32f164bab635c5a1d9
Artem Davydchenkov <davydchenkov1@gmail.com>
2023-07-16 15:32:46 +0300

    update

--- js/app.js
+++ js/app.js
@@ -462,10 +462,8 @@ const menu = () => {
   const catalogBack = document.querySelectorAll('.catalog-item__back');
   function disableScroll() {
     let pagePosition = window.scrollY;
-    let paddingOffset = window.innerWidth - document.body.offsetWidth + 'px';
     document.body.classList.add('scroll');
     document.body.dataset.position = pagePosition;
-    document.body.style.paddingRight = paddingOffset;
   }
   function enableScroll() {
     document.body.classList.remove('scroll');
@@ -505,7 +503,60 @@ const menu = () => {
   });
 };
 /* harmony default export */ const components_menu = (menu);
+;// CONCATENATED MODULE: ./source/js/components/select.js
+const initSelects = () => {
+  const SELECT_SELECTOR = '.js-select';
+  const BTN_SELECTOR = '.js-select-btn';
+  const LIST_SELECTOR = '.js-select-list';
+  const OPTION_SELECTOR = '.js-select-option';
+  const CLASS_ACTIVE = 'active';
+  const SELECTS = document.querySelectorAll('.js-select');
+  if (!SELECTS.length) return;
+  function closeAllSelect() {
+    const btnList = document.querySelectorAll(BTN_SELECTOR);
+    const selectList = document.querySelectorAll(LIST_SELECTOR);
+    btnList.forEach(el => el.classList.remove(CLASS_ACTIVE));
+    selectList.forEach(el => el.classList.remove(CLASS_ACTIVE));
+  }
+  SELECTS.forEach(select => {
+    const btn = select.querySelector(BTN_SELECTOR);
+    const selectList = select.querySelector(LIST_SELECTOR);
+    const optionList = selectList.querySelectorAll(OPTION_SELECTOR);
+    btn.addEventListener('click', e => {
+      const target = e.target.closest(BTN_SELECTOR);
+      if (target.classList.contains(CLASS_ACTIVE)) {
+        target.classList.remove(CLASS_ACTIVE);
+        selectList.classList.remove(CLASS_ACTIVE);
+      } else {
+        closeAllSelect();
+        target.classList.add(CLASS_ACTIVE);
+        selectList.classList.add(CLASS_ACTIVE);
+      }
+    });
+    selectList.addEventListener('click', e => {
+      const target = e.target.closest(OPTION_SELECTOR);
+      if (target) {
+        const value = target.getAttribute('data-value');
+        const content = target.innerHTML;
+        optionList.forEach(option => option.classList.remove(CLASS_ACTIVE));
+        target.classList.add(CLASS_ACTIVE);
+        btn.classList.remove(CLASS_ACTIVE);
+        btn.innerHTML = content;
+        btn.setAttribute('data-value', value);
+        selectList.classList.remove(CLASS_ACTIVE);
+      }
+    });
+  });
+  document.addEventListener('click', e => {
+    const target = e.target;
+    if (target && !target.closest(SELECT_SELECTOR)) {
+      closeAllSelect();
+    }
+  });
+};
+/* harmony default export */ const components_select = (initSelects);
 ;// CONCATENATED MODULE: ./source/js/index.js
+
 
 
 
@@ -517,6 +568,7 @@ function init() {
   components_menu();
   input();
   modal.init();
+  components_select();
   function footerMenu() {
     const dropDown = document.querySelectorAll('.footer__item');
     dropDown.forEach(el => {
